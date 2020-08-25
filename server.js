dd6db8207a751bda1f7cacb6ab2d021a9e6a947c
Valerii Kuzin <kuzin.valeriy@gmail.com>
2020-08-26 00:42:05 +0300

    edited

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
-const port = process.env.PORT || 8080;
 
 const app = express();
-app.use(favicon(__dirname + '/build/favicon.png'));
+app.use(favicon(__dirname + '/build/favicon.ico'));
 
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
@@ -12,4 +11,4 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(port);
+app.listen(9000);
